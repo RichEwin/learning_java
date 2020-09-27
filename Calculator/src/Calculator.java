444e8fc59a4c82d560eee89416bd0d7a76f37e00
Richard Ewin-Lambert <richardewinlambert@gmail.com>
2020-09-27 09:34:56 +0100

    Add files via upload

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public static void addition(int a, int b) {
+        System.out.println("Your number is " + (a + b));
+    }
+
+    public static void subtraction(int a, int b) {
+        System.out.println("Your number is " + (a - b));
+    }
+
+    public static void multiplication(int a, int b) {
+        System.out.println("Your number is " + (a * b));
+    }
+
+    public static void division(int a, int b) {
+        System.out.println("Your number is " + (a / b));
+    }
+
+}
---
+++ Calculator/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator.addition(5, 5);
+        Calculator.subtraction(100, 50);
+        Calculator.multiplication(5,5);
+        Calculator.division(10, 5);
+
+    }
+
+}
