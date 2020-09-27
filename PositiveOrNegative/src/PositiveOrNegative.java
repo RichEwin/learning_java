e899078d6f324f27bba0e9bc0b38e735ad0b16f9
Richard Ewin-Lambert <richardewinlambert@gmail.com>
2020-09-27 08:50:34 +0100

    Add files via upload

---
+++ PositiveOrNegative/src/PositiveOrNegative.java
@@ -0,0 +1,21 @@
+public class PositiveOrNegative {
+
+    public static void main(String[] args) {
+
+        checkNumber(1);
+        checkNumber(-1);
+        checkNumber(0);
+
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else if (number == 0) {
+            System.out.println("zero");
+            }
+        }
+    }
+
