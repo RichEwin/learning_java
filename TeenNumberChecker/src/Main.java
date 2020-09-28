74eefe6975dd4260cf4e970804a69a639581de0c
Richard Ewin-Lambert <richardewinlambert@gmail.com>
2020-09-28 11:53:18 +0100

    Add files via upload

---
+++ TeenNumberChecker/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(TeenNumberChecker.hasTeen(1, 1, 1));
+        System.out.println(TeenNumberChecker.hasTeen(23, 15, 42));
+        System.out.println(TeenNumberChecker.hasTeen(22, 23, 34));
+
+        System.out.println(TeenNumberChecker.isTeen(9));
+        System.out.println(TeenNumberChecker.isTeen(13));
+
+    }
+}
