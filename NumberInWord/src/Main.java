5cce1dd3ce87a9f17a7a455fbb30376e09c8b426
Richard Ewin-Lambert <richardewinlambert@gmail.com>
2020-09-29 12:12:05 +0100

    Add files via upload

---
+++ NumberInWord/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        NumberInWord.printNumberInWord(6);
+        NumberInWord.printNumberInWord(-1);
+        NumberInWord.printNumberInWord(1);
+        NumberInWord.printNumberInWord(7);
+    }
+}
